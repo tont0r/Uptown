31e78b2f8df845178c5749337b3958a01ba526b2
tont0r <marcel.walden@live.com>
2016-03-26 19:40:48 -0400

    adding all this other shit

---
+++ Assets/ActivateFlowchart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class ActivateFlowchart : MonoBehaviour
+{
+
+    public Flowchart flowChart;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+            return;
+        flowChart.SetStringVariable("interacting", gameObject.tag);
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+            return;
+        flowChart.SetStringVariable("interacting", "");
+
+    }
+}
---
+++ Assets/scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+
+    private Camera gameCamera;
+    void Start()
+    {
+        gameCamera = GetComponent<Camera>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            
+            Vector3 point = gameCamera.WorldToViewportPoint(target.position);
+            Vector3 delta = new Vector3(target.position.x,0,0) - gameCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = (destination);
+        }
+
+    }
+}
